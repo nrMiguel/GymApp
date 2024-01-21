ac8b45ee754675108f40ee3fad04085560d2a165
miguelnunezramon1 <miquel1993@hotmail.com>
2024-01-21 12:14:42 +0100

    Juntada la rama 'feature-musculo_activity' con 'develop_musculosDeGrupos'

--- app/src/main/java/com/example/mygymbroapp/providers/GrupoMuscularProvider.kt
+++ app/src/main/java/com/example/mygymbroapp/providers/GrupoMuscularProvider.kt
@@ -13,7 +13,6 @@ class GrupoMuscularProvider {
             GrupoMuscular("Antebrazos", "https://previews.123rf.com/images/decade3d/decade3d1307/decade3d130700106/20869752-antebrazos-anatom%C3%ADa-m%C3%BAsculos.jpg"),
             GrupoMuscular("Trapecios", "https://i.ytimg.com/vi/G3ZgZxTK0es/maxresdefault.jpg"),
             GrupoMuscular("Triceps", "https://images.hola.com/imagenes/estar-bien/20210311185849/ejercicios-triceps-fortalecer-brazos/0-929-371/ejercicios-triceps5-a.jpg"),
-            //TODO: Debería poner dentro de Espalda Trapecios, Dorsales y Redondos(Este no está puesto en este provider)? o mejor dejar por separado?
             GrupoMuscular("Espalda", "https://i.pinimg.com/736x/26/3d/d9/263dd97602d571593f96fc588b6ffa6a.jpg"),
             GrupoMuscular("Gluteos", "https://somasalud.es/wp-content/uploads/2019/03/musculo-gluteo-1536x864.jpg"),
             GrupoMuscular("Piernas", "https://findyoureverest.es/wp-content/uploads/2020/04/musculos-de-la-pierna-1024x768.jpg")
